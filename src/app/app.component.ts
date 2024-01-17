2fdd99a0d271e03dec27220619d2d548814df013
LVidalX <luisvidalpuma@gmail.com>
2024-01-16 20:47:04 -0500

    Criatura Mitologica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component';
+import { CabezaComponent } from './cabeza/cabeza.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, PrincipalComponent, CabezaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
